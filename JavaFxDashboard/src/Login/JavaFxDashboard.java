d280ea9d9aa645c541d1e5f0877e9b6a0bf23d78
CesarsEren <cpinedo428@gmail.com>
2018-08-03 07:16:06 -0500

    Diseño de Login
    Inicio Login Solo diseño solo Etiquetado XML

--- JavaFxDashboard/src/Login/JavaFxDashboard.java
+++ JavaFxDashboard/src/Login/JavaFxDashboard.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxdashboard;
+package Login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -22,7 +23,7 @@ public class JavaFxDashboard extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
